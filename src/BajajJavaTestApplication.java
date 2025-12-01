8eac0c5df72d8de2a5ed1bc64e41ea8975762440
sathesh-0714 <satheshkumaritraju@gmail.com>
2025-12-01 18:18:32 +0530

    Final Spring Boot submission

---
+++ src/BajajJavaTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.bajajfinancehealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BajajJavaTestApplication.class, args);
+    }
+}
